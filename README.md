0c4a07d27606bc54870157e1a45eaf340e6b99b1
maxencejded <maxence.jded@gmail.com>
2019-07-09 13:24:47 -0700

    Update vimrc

--- README.md
+++ README.md
@@ -7,22 +7,9 @@ git clone https://github.com/maxencejded/macOS.git ~/.macOS
 ```
 
 ## Automatic Mode
-### Normal
 ```MAKEFILE
-sh .macOS/script.sh
-```
-### 42 Silicon Valley
-```MAKEFILE
-export ZSH="$HOME/.macOS/oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sh .macOS/script.sh
 ```
 After go to iTerm > Preferences > Profils and change the Colors Dracula and the Text > Font by `14pt
 Roboto Mono for Powerline`
 
-### Requirements
-* Plugin\
--> https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
-
-## To Do
-Make a script to automate everything
--- script.sh
+++ script.sh
@@ -1,64 +1,20 @@
 #!/bin/bash
 
 VIMRC=.vimrc
-ZSHRC=.zshrc
-BACKUPDIR=Backup
 COLOiRRED="\033[31m"
 COLORGREEN="\033[32m"
 COLOREDEND="\033[0m"
-INTRA="42.us.org"
 
-# Install brew
-if [ ! -d $HOME/.brew ]; then
-	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-# Create backup folder
-[[ -d dir ]] || mkdir .macOS/$BACKUPDIR/
-
-# VIM
-
+# vim
 printf "VIM Prefrences\n"
 if [ -f $VIMRC ]; then
 	printf "Backup your $VIMRC"
-	mv $VIMRC .macOS/$BACKUPDIR/$VIMRC
+	mv $VIMRC "$VIMRC"_old
 	printf "$COLORGREEN\tDone!$COLOREDEND\n"
 fi
 
-ln -s .macOS/vim/vimrc $VIMRC
-
-# ZSHRC
-
-# Add them
-
-if [[ $HOSTNAME = *$INTRA* ]] &&  [ ! -d $ZSH/themes/spaceship-prompt ]; then
-	git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/themes/spaceship-prompt"
-	ln -s "$ZSH/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/themes/spaceship.zsh-theme"
-else
-	if [ -d .npm ]; then
-		npm install -g spaceship-prompt
-	else
-		brew install npm
-		npm install -g spaceship-prompt
-		npm uninstall npm -g
-		rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/npm*
-	fi
-fi
-
-printf "ZSH Prefrences\n"
-if [ -f $ZSHRC ]; then
-	printf "Backup your $ZSHRC"
-	mv $ZSHRC .macOS/$BACKUPDIR/$ZSHRC
-	printf "$COLORGREEN\tDone!$COLOREDEND\n"
-fi
-
-if [[ $HOSTNAME = *$INTRA* ]]; then
-	ln -s .macOS/zsh/zshrc42 $ZSHRC
-else
-	ln -s .macOS/zsh/zshrc $ZSHRC
-fi
-
-# iTerm
+mv .macOS/vim ~/.vim
+ln -s .vim/vimrc $VIMRC
 
 # Add the fonts
 git clone https://github.com/powerline/fonts.git --depth=1
